503d91a78a854b405b929b13ddde898184eb6734
vinaykumar785557 <c0785557@mylambton.ca>
2022-04-22 18:21:36 -0400

    pushed after firebase deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Footer from './Footer';
+import Header from './Header';
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchPage from './SearchPage';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='app'>
+			{/* Home --> Header, Banner + Search Dates + Cards + Footer */}
+			{/* Header */}
+			{/* Banner + Search Dates */}
+			{/* Cards */}
+			{/* Footer */}
+			{/* Search Page */}
+			{/* Header */}
+
+			<Router>
+				<Header />
+				<Switch>
+					<Route path='/search'>
+						<SearchPage />
+					</Route>
+					<Route path='/' exact>
+						<Home />
+					</Route>
+				</Switch>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+function Footer() {
+	return (
+		<div className='footer'>
+			<p>
+				© {new Date().getFullYear()} Airbnb clone! No rights reserved - this is
+				a demo!
+			</p>
+			<p>Privacy · Terms · Sitemap · Company Details</p>
+		</div>
+	);
+}
+
+export default Footer;
